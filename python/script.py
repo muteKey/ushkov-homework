d65e68ae55aa114c737b12c4b02bd77c06d114a7
Kirill Ushkov <kirillushkov2010@gmail.com>
2021-05-18 12:36:49 +0300

    fixed PR comments

--- python/script.py
+++ python/script.py
@@ -4,43 +4,50 @@ import json
 from yaml import safe_load as yaml_load
 import boto3
 
-yml_title = "ec2_config.yml"
-json_title = "ec2_config.json"
 
-json_config_exists = os.path.isfile(json_title)
-yaml_config_exists = os.path.isfile(yml_title)
+def main():    
+    yml_title = "ec2_config.yml"
+    json_title = "ec2_config.json"
 
-config = {}
+    json_config_exists = os.path.isfile(json_title)
+    yaml_config_exists = os.path.isfile(yml_title)
 
-if json_config_exists:
-    with open(json_title) as json_file:
-        config = json.load(json_file)
-elif yaml_config_exists:
-    with open(yml_title) as yml_file:
-        config = yaml_load(yml_file)
-else:
-    sys.exit("No config file found: checked for ec2_config.json or ec2_config.yml")
+    config = {}
 
-if config.get("Instances"):
-    instances_info = config.get("Instances")
-    ec2 = boto3.client('ec2')
-    for instance_info in instances_info:
-        ami = instance_info["ami-id"]
-        instance_tags = instance_info["tags"]
-        tags = []
+    if json_config_exists:
+        with open(json_title) as json_file:
+            config = json.load(json_file)
+    elif yaml_config_exists:
+        with open(yml_title) as yml_file:
+            config = yaml_load(yml_file)
+    else:
+        sys.exit("No config file found: checked for ec2_config.json or ec2_config.yml")
 
-        for dict in instance_tags:
-            for key in dict.keys():
-                res = {"Key": key, "Value": dict[key] }
-                tags.append(res)
-        
-        tag_spec = [{"ResourceType": "instance", "Tags": tags}]
-        instance_type = instance_info["instance-type"]
+    if config.get("Instances"):
+        instances_info = config.get("Instances")
+        ec2 = boto3.client('ec2')
+        for instance_info in instances_info:
+            ami = instance_info["ami-id"]
+            instance_tags = instance_info["tags"]
+            tags = []
 
-        if instance_type.startswith("t2"):
-            print("WARNING: You use old instance type - {}".format(instance_type))
+            for dict in instance_tags:
+                for key in dict.keys():
+                    res = {"Key": key, "Value": dict[key] }
+                    tags.append(res)
+            
+            tag_spec = [{"ResourceType": "instance", "Tags": tags}]
+            instance_type = instance_info["instance-type"]
 
-        instance = ec2.run_instances(InstanceType=instance_type, ImageId=ami, TagSpecifications=tag_spec, MaxCount=1, MinCount=1)
-        
-else:
-    sys.exit("Incorrect config: Instances not specified")
+            if instance_type.startswith("t2"):
+                print("WARNING: You use old instance type - {}".format(instance_type))
+
+            instance = ec2.run_instances(InstanceType=instance_type, ImageId=ami, TagSpecifications=tag_spec, MaxCount=1, MinCount=1)
+            
+    else:
+        sys.exit("Incorrect config: Instances not specified")
+
+
+if __name__ == "__main__":
+    # execute only if run as a script
+    main()
